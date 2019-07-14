4ccaffccdc7681fffb0a53bffbee5594a0de66ad
Lyss <lyssfitz@outlook.com>
2019-07-14 16:47:16 +1000

    Add user comment component

---
+++ src/components/UserComment/UserComment.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class UserComment extends Component {
+  render() {
+    return (
+      <p>Enter a comment about the article...</p>
+    );
+  }
+}
+
+export default UserComment;
