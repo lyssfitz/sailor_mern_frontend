c13014dfe1d1b9e434495cb432b08fbd6c672b69
Alex <atdiwa@gmail.com>
2019-07-15 16:38:41 +1000

    added global state for articles/feed and get data from json server

--- src/components/pages/FeedPage.js
+++ src/components/pages/FeedPage.js
@@ -1,21 +1,39 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 // import InterestsModal from "./InterestsModal"
-import { showModal } from "./../../actions"
+import { showModal, fetchArticles } from "./../../actions"
 
 class FeedPage extends Component {
   componentDidMount = () => {
-    this.props.showModal();
+    // this.props.showModal();
+    this.props.fetchArticles();
   }
 
   render() {
+    const { articles } = this.props;
+    // console.log(articles);
+    console.log(this.props);
     return (
       <>
+        {/* Hide Interests Modal during development */}
         {/* <InterestsModal /> */}
         <h1>Feed</h1>
+        <ul>
+          {articles.map((article, index) => {
+            return (
+              <li key={article.metadata.title}>{article.date_posted}</li>
+            );
+          })}
+        </ul>
       </>
     );
   }
 }
 
-export default connect(null, { showModal })(FeedPage);
+const mapStateToProps = (state) => {
+  return {
+    articles: state.articles
+  }
+}
+
+export default connect(mapStateToProps, { showModal, fetchArticles })(FeedPage);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import authReducer from "./auth_reducer";
 import modalReducer from "./modal_reducer"
 import interestsReducer from "./interests_reducer"
+import articlesReducer from "./articles_reducer"
 import { reducer as formReducer } from "redux-form";
 
 export default combineReducers({
   auth: authReducer,
   modal: modalReducer,
   interests: interestsReducer,
+  articles: articlesReducer,
   form: formReducer
 });
