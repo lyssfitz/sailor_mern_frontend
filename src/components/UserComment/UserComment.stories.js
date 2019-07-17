f3244a4be6f9ffca0e68f12df0d9ba216d7883e9
Lyss <lyssfitz@outlook.com>
2019-07-17 10:28:28 +1000

    Update user comments and mentions

--- src/components/UserComment/UserComment.stories.js
+++ src/components/UserComment/UserComment.stories.js
@@ -3,13 +3,19 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import UserComment from 'components/UserComment/UserComment.jsx'
 
+const Users = [
+  "afc163",
+  "zombieJ",
+  "yesmeck",
+]
+
 storiesOf('UserComment', module)
   .addDecorator(story => <div style={{padding: '3rem'}}>{story()}</div>)
   .add('default', () => (
     <UserComment/>
   ))
   .add('with entry', () => (
-    <UserComment comment="This article is an execellent read"/>
+    <UserComment users={Users} comment="This article is an excellent read"/>
   ))
   .add('with entry and mention', () => (
     <UserComment comment="@john I think you should read this"/>
