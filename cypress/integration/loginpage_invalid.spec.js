e087e95b419482f6f2f435bf18925e2d4a3ba7f1
Alex <atdiwa@gmail.com>
2019-07-17 20:37:47 +1000

    added cypress tests for success and fail login

---
+++ cypress/integration/loginpage_invalid.spec.js
@@ -0,0 +1,43 @@
+describe('Login Form with invalid email', () => {
+  it("visits the login page", () => {
+    cy.visit("/login");
+  })
+
+  it("adds an invalid email ", () => {
+    cy.get('input[type=email]').type('loginsdfsdf@test.com')
+  })
+
+  it("adds a password", () => {
+    cy.get('input[type=password]').type('testpass')
+  })
+
+  it("clicks the sign up button", () => {
+    cy.get('button').click();
+  })
+
+  it("redirects user to login", () => {
+    cy.url().should('include', '/login');
+  })
+})
+
+describe('Login Form with invalid password', () => {
+  it("visits the login page", () => {
+    cy.visit("/login");
+  })
+
+  it("adds an invalid email ", () => {
+    cy.get('input[type=email]').type('login@test.com')
+  })
+
+  it("adds a password", () => {
+    cy.get('input[type=password]').type('testpass1')
+  })
+
+  it("clicks the sign up button", () => {
+    cy.get('button').click();
+  })
+
+  it("redirects user to login", () => {
+    cy.url().should('include', '/login');
+  })
+})
