5a6ffe10a324b7177b06aad6bbf4f4aa2ec3d8e6
Lyss <lyssfitz@outlook.com>
2019-07-17 15:09:59 +1000

    Start comment component

---
+++ src/components/UserComment/UserComment.stories.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import { storiesOf } from '@storybook/react';
+// import { action } from '@storybook/addon-actions';
+// import UserComment from 'components/UserComment/UserComment.jsx'
+
+// const Users = [
+//   "afc163",
+//   "zombieJ",
+//   "yesmeck",
+// ]
+
+// storiesOf('UserComment', module)
+//   .addDecorator(story => <div style={{padding: '3rem'}}>{story()}</div>)
+//   .add('default', () => (
+//     <UserComment/>
+//   ))
+//   .add('with entry', () => (
+//     <UserComment users={Users} comment="This article is an excellent read"/>
+//   ))
+
+// ;
