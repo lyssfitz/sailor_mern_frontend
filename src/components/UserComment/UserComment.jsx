91c26463865d826a7d918e817ef6848401c3aa9f
Lyss <lyssfitz@outlook.com>
2019-07-19 14:05:10 +1000

    Add ability for user to add their own comment

--- src/components/UserComment/UserComment.jsx
+++ src/components/UserComment/UserComment.jsx
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
-import { Comment, Tooltip, List } from 'antd';
+import { Comment, Avatar, Form, Button, List, Input, Tooltip } from 'antd';
 import moment from 'moment';
 
+const { TextArea } = Input;
 
+const CommentList = ({ comments }) => (
+  <List
+    dataSource={comments}
+    header={`${comments.length} ${comments.length > 1 ? 'replies' : 'reply'}`}
+    itemLayout="horizontal"
+    renderItem={props => <Comment {...props} />}
+  />
+);
+
+const Editor = ({ onChange, onSubmit, submitting, value }) => (
+  <div>
+    <Form.Item>
+      <TextArea rows={4} onChange={onChange} value={value} />
+    </Form.Item>
+    <Form.Item>
+      <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
+        Add Comment
+      </Button>
+    </Form.Item>
+  </div>
+);
 
 const data = [
   {
@@ -58,6 +80,43 @@ const data = [
 ];
 
 class UserComment extends Component {
+  state = {
+    comments: [],
+    submitting: false,
+    value: '',
+  };
+
+  handleSubmit = () => {
+    if (!this.state.value) {
+      return;
+    }
+
+    this.setState({
+      submitting: true,
+    });
+
+    setTimeout(() => {
+      this.setState({
+        submitting: false,
+        value: '',
+        comments: [
+          {
+            author: 'Han Solo',
+            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            content: <p>{this.state.value}</p>,
+            datetime: moment().fromNow(),
+          },
+          ...this.state.comments,
+        ],
+      });
+    }, 1000);
+  };
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
 
   render() {
     return (
