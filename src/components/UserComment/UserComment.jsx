bbe4872a76f77338497d8e6f9230c0ce558c4907
Lyss <lyssfitz@outlook.com>
2019-07-19 15:49:09 +1000

    Refactor comment data

--- src/components/UserComment/UserComment.jsx
+++ src/components/UserComment/UserComment.jsx
@@ -2,19 +2,19 @@ import React, { Component } from "react";
 import { Comment, Tooltip } from 'antd';
 import moment from 'moment';
 
+const authorName = "Mary Smith";
+const content = "That was an excellent article!";
+const dateTime = moment();
+
 class UserComment extends Component {
   render() {
     return (
       <Comment
-        author={<h4>Han Solo</h4>}
-        content={
-          <p>
-            That was an excellent article!
-          </p>
-        }
+        author={<h4>{authorName}</h4>}
+        content={<p>{content}</p>}
         datetime={
-          <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
-            <span>{moment().fromNow()}</span>
+          <Tooltip title={dateTime.format('YYYY-MM-DD HH:mm:ss')}>
+            <span>{dateTime.fromNow()}</span>
           </Tooltip>
         }
       />
